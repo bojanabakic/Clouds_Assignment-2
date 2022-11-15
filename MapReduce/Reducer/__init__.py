1325bee00fd7291f07dfadfebf49d4a0735b1369
Bojana Bakić <bojanabakic96@gmail.com>
2022-11-15 16:31:25 +0200

    Add files via upload

---
+++ MapReduce/Reducer/__init__.py
@@ -0,0 +1,15 @@
+# This function is not intended to be invoked directly. Instead it will be
+# triggered by an orchestrator function.
+# Before running this sample, please:
+# - create a Durable orchestration function
+# - create a Durable HTTP starter function
+# - add azure-functions-durable to requirements.txt
+# - run pip install -r requirements.txt
+
+import logging
+
+
+def main(pairs: tuple) -> tuple:
+    word = pairs[0]
+    occurance = sum(pairs[1])
+    return (word, occurance)
